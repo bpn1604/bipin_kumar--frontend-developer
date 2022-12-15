49343c6bb1406d8b5149a01bd72a720f1a11510e
bpn1604 <bipin.kumar2015@vitalum.ac.in>
2022-12-15 18:31:40 +0530

    almost compl

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,25 +1,49 @@
-import { Box, Flex, Grid, GridItem, Input, SimpleGrid } from "@chakra-ui/react";
+import { Box, Button, Flex, Grid, GridItem, Input, SimpleGrid } from "@chakra-ui/react";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import styles from "./Home.module.css";
 import SingleCard from "./SingleCard";
 const Home = () => {
   const [data, setData] = useState([]);
-  const [type, setType] = useState("");
+  const [text, setText] = useState("");
+  const [page, setPage] = useState(1);
   
   useEffect(() => {
-    axios.get(`https://api.spacexdata.com/v3/capsules`).then((res) => {
+    axios.get(`https://api.spacexdata.com/v3/capsules?user_id=714830764&start=2017-06-22&end=2017-06-25&type=${text}&offset=${page}&page_size=10&rfq_status=Published`).then((res) => {
       
       setData(res.data);
     });
   }, []);
+  const handlesearch = () => {
+    axios
+      .get(
+        `https://apitest.vendosmart.com//seller/seller_rfq_list?user_id=714830764&start_date=&end_date=&search_key=${text}&page_number=1&page_size=10&rfq_status=Published`
+      )
+      .then((res) => setData(res.data.rfq_list));
+  };
   return (
     <>
-    <Flex margin="20px">
-    <Input />
-    <Input />
-    <Input />
-    </Flex>
+    <Input
+        htmlSize={4}
+        width="auto"
+        onChange={(e) => setText(e.target.value)}
+        placeholder="Type your text"
+      />
+      <Input
+        placeholder="Select Date and Time"
+        size="sm"
+        type="datetime-local"
+        width="500px"
+        height="40px"
+      />
+      <Input
+        placeholder="Select Date and Time"
+        size="sm"
+        type="datetime-local"
+        width="500px"
+        height="40px"
+      />
+      <Button onClick={handlesearch}>Search</Button>
     
     <Grid templateColumns='repeat(8, 1fr)'   gap={2} margin="20px">
       {
@@ -65,6 +89,18 @@ const Home = () => {
   
   
 </Grid>
+<Flex justifyContent="flex-end" marginRight="10px" marginTop="10px">
+      <Button disabled={page===1} onClick={(e)=>setPage((page)=>page-1)}>Prev</Button>
+        ...
+        
+        <Button disabled={page===1} onClick={(e)=>setPage((page)=>page-1)} >{page-1}</Button>
+        .
+        <Button>{page}</Button>
+        .
+        <Button onClick={(e)=>setPage((page)=>page+1)}>{page+1}</Button>
+        ... <Button disabled={page===50} onClick={(e)=>setPage((page)=>page+1)}>Next</Button>
+        </Flex>
+    
     </>
   );
 };
